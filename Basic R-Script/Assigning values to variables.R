6ffad1588f932a2d347ae3e1b962b0ebce795724
Tathagata-Mukherjee <tathagatamukherjee.875@gmail.com>
2024-02-13 19:47:48 +0530

    Assigning values to variables

---
+++ Basic R-Script/Assigning values to variables.R
@@ -0,0 +1,10 @@
+#Assigning values into variables
+
+a<-5
+b<-5
+c<-a+b
+print(c)   #prints 10
+c          #prints 10
+
+print("Addition = ", c)    #doesn't print the value
+paste("Addition = ", c)    #prints 10
