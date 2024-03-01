cb41987e05761b1c47708d3aa26adbdb1e6de6dd
Tathagata-Mukherjee <tathagatamukherjee.875@gmail.com>
2024-03-01 14:50:32 +0530

    For Loops

---
+++ Basic R-Script/For Loops.R
@@ -0,0 +1,23 @@
+#Iteration
+#For Loop
+
+for(i in seq(1,10)){
+  print(i)
+}
+
+for(a in seq(11,50,by=2)) {
+  print(a)
+}
+
+#Displaying Range two times
+v1<-c(11:20)
+for(b in rep(v1,each=2)){
+  print(b)
+}
+
+#OR
+
+for(b in seq(11,20)){
+  print(rep(b,2))
+}
+
