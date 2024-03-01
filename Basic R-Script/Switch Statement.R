161cd8a36145ff57de74612d2658c820843827c1
Tathagata-Mukherjee <tathagatamukherjee.875@gmail.com>
2024-03-01 13:19:14 +0530

    Switch Statement

---
+++ Basic R-Script/Switch Statement.R
@@ -0,0 +1,12 @@
+#R switch statement
+#It is a multiway branch statement
+#Provide an easy way to dispatch execution for different part of the code
+#This code is based on the value of the expression
+#if expression type is character string
+#The string is matched to the listed cases
+#If there is more than one match
+#the first match element is used
+#No default case is available
+#If case is matched, an unnamed case is used
+#Two types based on index, based on matching values
+
