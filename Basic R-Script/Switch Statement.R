3d12f5bb68ee9e551809934ce71ed26fc72e9ffb
Tathagata-Mukherjee <tathagatamukherjee.875@gmail.com>
2024-03-01 13:31:34 +0530

    Switch Statement

--- Basic R-Script/Switch Statement.R
+++ Basic R-Script/Switch Statement.R
@@ -10,3 +10,18 @@
 #If case is matched, an unnamed case is used
 #Two types based on index, based on matching values
 
+
+#Type 1: Based on indexing
+#switch(expression as input,case1,case2,...)
+
+#Indexvalue   1  2  3  4  5  6
+z<-switch(2, 56,58,69,36,23,11)
+z                                 #58 is printed as it is on 2nd index
+
+z<-switch(8, 56,58,69,36,23,11)
+z                                 #Prints NULL as no value in 8th index
+
+
+#Type 2: Based on matching values
+#switch(expression as character,case1,case2,...,caseN)
+
